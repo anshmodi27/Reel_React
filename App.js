cade6f73491720700348532c24f826e52d0a0c91
Ansh Modi <121238681+Anshmodi30@users.noreply.github.com>
2023-03-27 17:03:47 +0000

    ...

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { Video } from "expo-av";
 
-export default function App() {
+const App = () => {
+  const videoRef = React.useRef(null);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Video
+        ref={videoRef}
+        // source={{
+        //   uri: "https://youtu.be/cEvsnYt3FIk?t=3207",
+        // }}
+        source={{
+          uri: "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+        }}
+        style={styles.backgroundVideo}
+        rate={1}
+        shouldPlay={true}
+        useNativeControls={true}
+        volume={1}
+        muted={false}
+        resizeMode="cover"
+      />
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // flex: 1,
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  backgroundVideo: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   },
 });
